6b4d43e932155883f0eab5df3d746d9431ac0dd5
zizhao <zizhao@nvidia.com>
2023-05-10 06:12:45 +0300

    yield in loop

--- src/main/scala/org/apache/spark/shuffle/compat/spark_2_4/UcxShuffleClient.scala
+++ src/main/scala/org/apache/spark/shuffle/compat/spark_2_4/UcxShuffleClient.scala
@@ -9,7 +9,6 @@ import org.apache.spark.storage.{BlockId => SparkBlockId, ShuffleBlockId => Spar
 
 class UcxShuffleClient(val transport: UcxShuffleTransport) extends ShuffleClient{
   val worker = transport.selectLocalWorker()
-  var numFetched = 0
   override def fetchBlocks(host: String, port: Int, execId: String, blockIds: Array[String],
                            listener: BlockFetchingListener,
                            downloadFileManager: DownloadFileManager): Unit = {
@@ -27,7 +26,6 @@ class UcxShuffleClient(val transport: UcxShuffleTransport) extends ShuffleClient
             this
           }
         })
-        numFetched += 1
       }
     }
     val resultBufferAllocator = (size: Long) => transport.hostBounceBufferMemoryPool.get(size)
@@ -39,11 +37,8 @@ class UcxShuffleClient(val transport: UcxShuffleTransport) extends ShuffleClient
   }
 
   def progress(): Unit = {
-    numFetched = 0
-    while (numFetched == 0) {
-      if (worker.worker.progress() == 0) {
-        Thread.`yield`()
-      }
+    while (worker.worker.progress() == 0) {
+      Thread.`yield`()
     }
   }
 }
