a00095c12c77ccbf6ae598f6d6c62c5305bbb9b4
zizhao <zizhao@nvidia.com>
2023-06-14 10:26:28 +0300

    add preconnection

--- src/main/scala/org/apache/spark/shuffle/ucx/UcxShuffleTransport.scala
+++ src/main/scala/org/apache/spark/shuffle/ucx/UcxShuffleTransport.scala
@@ -219,11 +219,12 @@ class UcxShuffleTransport(var ucxShuffleConf: UcxShuffleConf = null, var executo
   def addExecutors(executorIdsToAddress: Map[ExecutorId, SerializableDirectBuffer]): Unit = {
     executorIdsToAddress.foreach {
       case (executorId, address) => executorAddresses.put(executorId, address.value)
+      allocatedClientWorkers.foreach(_.getConnection(executorId))
     }
   }
 
-  def preConnect(): Unit = {
-    allocatedClientWorkers.foreach(_.preconnect())
+  def preConnect(): Unit = { 
+    allocatedClientWorkers.foreach(_.progressConnect)
   }
 
   /**
