8f2be9b9008fce15846af15c4d5303e95aa8043a
zizhao <zizhao@nvidia.com>
2024-01-08 17:05:19 +0200

    Revert "preconnect before shuffle"
    This reverts commit a012c6f91aa9767890b493c8f3a9d76adbf75f79.

--- src/main/scala/org/apache/spark/shuffle/ucx/UcxShuffleTransport.scala
+++ src/main/scala/org/apache/spark/shuffle/ucx/UcxShuffleTransport.scala
@@ -208,8 +208,7 @@ class UcxShuffleTransport(var ucxShuffleConf: UcxShuffleConf = null, var executo
     executorIdsToAddress.foreach {
       case (executorId, address) => executorAddresses.put(executorId, address.value)
     }
-    allocatedClientWorkers.foreach(w => executorIdsToAddress.foreach(
-      x => w.getConnection(x._1)))
+    allocatedClientWorkers.foreach(w => executorIdsToAddress.foreach(w.getConnection(_)))
   }
 
   def preConnect(): Unit = {
